d365608bbbc5f74d22b6af5766772a00a50ec3a6
klot3n <klot3n@gmail.com>
2020-11-01 11:32:03 +0500

    3nd lesson

--- app/src/main/java/com/klot3n/myapplication/MainActivity.kt
+++ app/src/main/java/com/klot3n/myapplication/MainActivity.kt
@@ -2,24 +2,28 @@ package com.klot3n.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import android.widget.Toolbar
 import com.klot3n.myapplication.databinding.ActivityMainBinding
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 
 private lateinit var mBinding: ActivityMainBinding
 private lateinit var mDrawer: Drawer
 private lateinit var mHeader: AccountHeader
-private lateinit var mToolbar:androidx.appcompat.widget.Toolbar
+private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding= ActivityMainBinding.inflate(layoutInflater)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
     }
 
@@ -30,13 +34,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initFunc() {
-       setSupportActionBar(mToolbar)
+        setSupportActionBar(mToolbar)
         createHeader()
         createDrawer()
     }
 
     private fun createDrawer() {
-        mDrawer=DrawerBuilder()
+        mDrawer = DrawerBuilder()
             .withActivity(this)
             .withToolbar(mToolbar)
             .withActionBarDrawerToggle(true)
@@ -47,12 +51,64 @@ class MainActivity : AppCompatActivity() {
                     .withIconTintingEnabled(true)
                     .withName("Создать группу")
                     .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_groups),
+                PrimaryDrawerItem().withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать секретный чат")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_secret_chat),
+                PrimaryDrawerItem().withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать канал")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_channel),
+                PrimaryDrawerItem().withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName("Контакты")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_contacts),
+                PrimaryDrawerItem().withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName("Звонки")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_phone),
+                PrimaryDrawerItem().withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName("Избранное")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_favorites),
+                PrimaryDrawerItem().withIdentifier(106)
+                    .withIconTintingEnabled(true)
+                    .withName("Настройки")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_settings),
+                DividerDrawerItem(),
+                PrimaryDrawerItem().withIdentifier(107)
+                    .withIconTintingEnabled(true)
+                    .withName("Пригласить друзей")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_invate),
+                PrimaryDrawerItem().withIdentifier(108)
+                    .withIconTintingEnabled(true)
+                    .withName("Вопросы о телеграм")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_help)
 
-            ).build()
+            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    Toast.makeText(this@MainActivity,position.toString(),Toast.LENGTH_SHORT).show()
+                    return false
+                }
+
+            }).build()
     }
 
     private fun createHeader() {
-        mHeader=AccountHeaderBuilder()
+        mHeader = AccountHeaderBuilder()
             .withActivity(this)
             .withHeaderBackground(R.drawable.header)
             .addProfiles(
@@ -62,7 +118,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initFields() {
-        mToolbar= mBinding.mainToolbar
+        mToolbar = mBinding.mainToolbar
 
 
     }
